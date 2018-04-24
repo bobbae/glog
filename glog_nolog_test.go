0433c5447896bb38895494c5c1d4778d35d80f9e
bobbae <bobbae@github.com>
2018-04-23 17:57:32 -0700

    first

---
+++ glog_nolog_test.go
@@ -0,0 +1,13 @@
+package glog
+
+import (
+	"testing"
+)
+
+func TestNoLog(t *testing.T) {
+	logging.toStderr = false
+	logging.verbosity.Set("2")
+	logging.nolog = true
+	Info("test -- should not create log or files")
+	logging.nolog = false
+}
